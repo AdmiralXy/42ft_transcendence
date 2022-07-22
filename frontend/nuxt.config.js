ab5006468235add908eaa5a185326cbc62776fbf
AdmiralXy <gameproblem80@gmail.com>
2022-07-22 22:03:20 +0300

    configure frontend babel

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -107,7 +107,11 @@ export default {
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    babel: {
+      compact: true
+    }
+  },
 
   // Google fonts
   googleFonts: {
